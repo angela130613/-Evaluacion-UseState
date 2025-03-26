5af3f4ab1b0590e74d563bd57af19e924e398c7b
andres <andres2345v@gmail.com>
2025-03-25 19:27:39 -0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
 import { useState } from 'react'
 import { Timer } from './assets/components/Layouts/Timer/Timer';
 import { LikeDislike } from './assets/components/Layouts/LikeDislike/LikeDislike';
+import { ColorGenerator } from './assets/components/Layouts/ColorGenerator/ColorGenerator';
 
 export const App = () => {
   // 1. Dark/Light Mode
   const [isDarkMode, setIsDarkMode] = useState(false);
-  
-
-  // 3. Color Selection
-  const coloresAbsolutos = [
-    "#FF0000", "#00FF00", "#0000FF", "#FFFF00", 
-    "#FF00FF", "#00FFFF", "#000000", "#FFFFFF", 
-    "#808080", "#800000", "#808000", "#800080", 
-    "#008080", "#C0C0C0", "#FFA500", "#A52A2A"
-  ];
-  const [selectedColor, setSelectedColor] = useState(null);
-
 
   // Toggle Dark/Light Mode
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
-
-  // Random Color Generator
-  const generateRandomColor = () => {
-    const randomColor = coloresAbsolutos[Math.floor(Math.random() * coloresAbsolutos.length)];
-    setSelectedColor(randomColor);
-  };
-
-  
-
-  
 
   return (
     <div className={`min-h-screen p-8 transition-all duration-300 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
@@ -48,22 +28,9 @@ export const App = () => {
         <LikeDislike />
 
         {/* 3. Color Generator */}
-        <div>
-          <h2 className="text-xl font-semibold mb-4">Color Generator</h2>
-          <button onClick={generateRandomColor} className="w-full py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition">
-            Generate Random Color
-          </button>
-          {selectedColor && (
-            <div className="mt-4 flex items-center justify-center">
-              <div className="w-40 h-40 rounded-lg shadow-md flex items-center justify-center"style={{ backgroundColor: selectedColor }}>
-                <span className="text-white font-bold">
-                  {selectedColor}
-                </span>
-              </div>
-            </div>
-          )}
-        </div>
+        <ColorGenerator />
 
+        {/* 4. Timer */}
         <Timer />
 
       </div>
--- src/assets/components/Layouts/ColorGenerator/ColorGenerator.jsx
+++ src/assets/components/Layouts/ColorGenerator/ColorGenerator.jsx
@@ -1,1 +1,35 @@
 import { useState } from 'react'
+
+export const ColorGenerator = () => {
+     // 3. Color Selection
+  const coloresAbsolutos = [
+    "#FF0000", "#00FF00", "#0000FF", "#FFFF00", 
+    "#FF00FF", "#00FFFF", "#000000", "#FFFFFF", 
+    "#808080", "#800000", "#808000", "#800080", 
+    "#008080", "#C0C0C0", "#FFA500", "#A52A2A"
+  ];
+  const [selectedColor, setSelectedColor] = useState(null);
+  // Random Color Generator
+  const generateRandomColor = () => {
+    const randomColor = coloresAbsolutos[Math.floor(Math.random() * coloresAbsolutos.length)];
+    setSelectedColor(randomColor);
+  };
+
+  return (
+    <div>
+          <h2 className="text-xl font-semibold mb-4">Color Generator</h2>
+          <button onClick={generateRandomColor} className="w-full py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition">
+            Generate Random Color
+          </button>
+          {selectedColor && (
+            <div className="mt-4 flex items-center justify-center">
+              <div className="w-40 h-40 rounded-lg shadow-md flex items-center justify-center"style={{ backgroundColor: selectedColor }}>
+                <span className="text-white font-bold">
+                  {selectedColor}
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+    )
+}
